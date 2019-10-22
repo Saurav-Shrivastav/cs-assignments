af79036c3a75beb911876e2ae46e610a55519422
csed <csed@localhost.localdomain>
2019-10-22 11:49:40 +0530

    trying second que of assi 7

---
+++ assi_7/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("enter the number of integers that you eish to check for being even or odd");
+	scanf("%d", n);
+	int arr1[n];
+	for (int i = 0; i < n; i++){
+		scanf("%d", &arr1[i]);
+	}
+	printf("%d", arr1[1]);
+	return 0;
+}
