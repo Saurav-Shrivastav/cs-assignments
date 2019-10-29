de7ecb492fda0653b0cc356d6d7a10ca855d147b
Saurav <sauravsrivastav103@gmail.com>
2019-10-29 23:52:57 +0530

    modified q1 of assi_2

--- assi_2/1.c
+++ assi_2/1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("The gross salary is %f", (50000*(1.8)));
+int main(){
+	int basic_salary = 50000;
+	int da = 0.5*basic_salary;
+	int hra = 0.2*basic_salary;
+	int va = 0.1*basic_salary;
+	int gross_pay = basic_salary + da + hra + va;
+	printf("%d", gross_pay);
+	return 0;
 }
